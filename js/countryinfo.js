3925c20b99d3d85aec88a1ce9381ad74215c445f
Jayakodehode <jaya.kodehode@gmail.com>
2023-09-01 11:52:43 +0200

    working with countryinfo.js

--- js/countryinfo.js
+++ js/countryinfo.js
@@ -4,9 +4,100 @@ const countryname = new URLSearchParams(window.location.search).get(`name`);
 
 fetch(`https://restcountries.com/v3.1/name/${countryname}`)
   .then((res) => res.json())
-  .then((country) => {
+  .then(([country]) => {
     console.log(country);
+
+    const flagsrc = country.flags.svg;
+    const capital = country.capital;
+    const name = countryname;
+    const nativeName = Object.values(country.name.nativeName)[0].common;
+    const population = country.population;
+    const region = country.region;
+    const subregion = country.subregion;
+    const currencies = Object.values(country.currencies)
+      .map((currency) => currency.name)
+      .join(", ");
+    const languages = Object.values(country.languages).join(", ");
+
+    Country(
+      flagsrc,
+      name,
+      capital,
+      nativeName,
+      region,
+      subregion,
+      currencies,
+      languages,
+      population
+    );
   });
+
+function Country(
+  flagsrc,
+  name,
+  capital,
+  nativename,
+  region,
+  subregion,
+  currencies,
+  languages,
+  population
+) {
+  const countryContainer = document.getElementById("country-container");
+
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img");
+
+  const countryFlagImg = document.createElement("img");
+  countryFlagImg.src = flagsrc;
+  countryFlagImg.classList.add("countryflag-image");
+  imgContainer.append(countryFlagImg);
+
+  const countryInfo = document.createElement("div");
+  countryInfo.classList.add("info");
+
+  const nameElement = document.createElement("h1");
+  nameElement.textContent = name;
+
+  // Create a container for additional information
+  const countryDetails = document.createElement("div");
+  countryDetails.classList.add("country-details");
+
+  const nativeName = document.createElement("p");
+  nativeName.textContent = `Native Name: ${nativename}`;
+
+  const capitalCity = document.createElement("p");
+  capitalCity.textContent = `Capital: ${capital}`;
+
+  const regionLabel = document.createElement("p");
+  regionLabel.textContent = `Region: ${region}`;
+
+  const subregionLabel = document.createElement("p");
+  subregionLabel.textContent = `Subregion: ${subregion}`;
+
+  const currenciesLabel = document.createElement("p");
+  currenciesLabel.textContent = `Currencies: ${currencies}`;
+
+  const languagesLabel = document.createElement("p");
+  languagesLabel.textContent = `Languages: ${languages}`;
+
+  const populationLabel = document.createElement("p");
+  populationLabel.textContent = `Population: ${population}`;
+
+  countryDetails.appendChild(nativeName);
+  countryDetails.appendChild(capitalCity);
+  countryDetails.appendChild(regionLabel);
+  countryDetails.appendChild(subregionLabel);
+  countryDetails.appendChild(currenciesLabel);
+  countryDetails.appendChild(languagesLabel);
+  countryDetails.appendChild(populationLabel);
+
+  countryInfo.appendChild(nameElement);
+  countryInfo.appendChild(countryDetails);
+
+  countryContainer.append(imgContainer, countryInfo);
+}
+
 /*const inputText = document.getElementById("input-text");
 const searchButton = document.getElementById("search-btn");
 
